7e88dd9ce1775a3d9450d17735f7b9c1139b8030
DaiboZhang <61837652+DaiboZhang@users.noreply.github.com>
2020-03-19 02:01:36 -0400

    Add files via upload
    Quick Data Access Layer (from Joe Oakes' code)

---
+++ TeamKegging/dal.py
@@ -0,0 +1,8 @@
+class dal:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+    def getHost(self):
+        return self.host
+    def getPort(self):
+        return self.port
