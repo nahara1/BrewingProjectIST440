db09dad03ccf4af2a005a5e5f79ba16d419b417c
Faith Gill <fya5021@psu.edu>
2020-03-18 20:10:25 -0400

    Added Spargingtank.py headers

---
+++ TeamMashing/SpargingTank.py
@@ -0,0 +1,7 @@
+# Project: Brewing Project
+# Purpose Details: Sparging tank transfer and automation
+# Course: IST 440W
+# Author: Team Mashing
+# Date Developed: 3/17/2020
+# Last Date Changed: 3/18/2020
+# Rev: 1.1
