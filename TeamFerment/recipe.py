cfeb93bed7f751c4d2372d713581f69af1c9bcda
annyespinal <anny_espinal@yahoo.com>
2020-03-18 23:29:04 -0400

    Team ferment recipe class

---
+++ TeamFerment/recipe.py
@@ -0,0 +1,41 @@
+
+#attributes
+_recipe_id = None
+_recipe_name = ""
+_recipe_volume = 0.0
+_recipe_yeast_storage_amt = 0.0
+_recipe_yeast = None
+_recipe_yeast_begin_temp = 0.0
+_recipe_grain = None
+_recipe_sparge_time = 0.0
+_recipe_sparge_temp = 0.0
+_recipe_wort_volume = 0.0
+_recipe_boil_temp = 0.0
+_recipe_boil_time = 0.0
+_recipe_add_hop_time = 0.0
+_recipe_hop_amt = 0.0
+_recipe_hops = None
+_recipe_after_boil_chill_temp = 0.0
+_recipe_ferment_time = 0.0
+_recipe_add_yeast_time = 0.0
+_recipe_add_yeast_temp = 0.0
+_recipe_ferment_temp = 0.0
+_recipe_wort_cool_temp = 0.0
+_recipe_carbonation = 0.0
+_recipe_bitter_units = 0.0
+_recipe_wort_cool_time = 0.0
+_recipe_wort_cool_temp = 0.0
+_recipe_ferment__cool_temp = 0.0
+
+
+#default constructor added
+class Recipe:
+    def __init__(self, id, name, volume, yeast, grain):
+        self.id = None
+        self.name = ""
+        self.volume = 0.0
+        self.yeast = None
+        self.grain = None
+
+
+
