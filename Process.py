a5b8db14c7062a5868dd8268609610fea1b5b8bc
Andrew <azs6196@psu.edu>
2020-03-18 20:19:14 -0400

    Added headers.

--- Process.py
+++ Process.py
@@ -1,3 +1,11 @@
+# Project: Brewing Project
+# Purpose Details: Enumerator for conditions.
+# Course: IST 440W
+# Author: Team Mashing
+# Date Developed: 3/17/2020
+# Last Date Changed: 3/18/2020
+# Rev: 1.1
+
 import enum
 
 class Process(enum.Enum):
