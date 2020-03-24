36be99346b059c2fbcae696989d20be4105e12bd
Michael <maa6200@psu.edu>
2020-03-24 11:05:17 -0400

    Added to HotLiquorTank.py

--- TeamMashing/HotLiquorTank.py
+++ TeamMashing/HotLiquorTank.py
@@ -3,6 +3,24 @@
 # Course: IST 440W
 # Author: Team Mashing
 # Date Developed: 3/17/2020
-# Last Date Changed: 3/18/2020
-# Rev: 1.0
+# Last Date Changed: 3/24/2020
+# Rev: 1.1
 
+class HotLiquorTank:
+    def __init__(self, tID, wVolume, wtemperature):
+        self.tank_ID = tID
+        self.water_amount = wVolume
+        self.water_temp = wtemperature
+        self.is_transferred = False
+
+    def get_tank_ID(self):
+        return self.tID
+
+    def get_water(self):
+        return self.wVolume
+
+    def send_hot_water(self):
+        return ("Hot water is sent.")
+
+    def check_water_temp(self):
+        return self.wtemperature
