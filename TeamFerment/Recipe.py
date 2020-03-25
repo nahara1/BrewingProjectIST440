743480a230f2d7c426e895a879c4bd3c4814afed
annyespinal <anny_espinal@yahoo.com>
2020-03-25 19:56:17 -0400

    updated constructor and added methods - AE

--- TeamFerment/Recipe.py
+++ TeamFerment/Recipe.py
@@ -9,12 +9,32 @@
 
 # default constructor added
 class Recipe:
-    def __init__(self, id, name, volume, yeast, grain):
+    def __init__(self, id, name, volume, yeast_storage_amt, yeast, yeast_begin_temp, grain, sparge_time, sparge_temp, wort_volume, boil_temp, boil_time, add_hop_time, hop_amt, hops, after_boil_chill_temp, ferment_time, add_yeast_time, add_yeast_temp, ferment_temp, wort_cool_temp, carbonation, bitter_units, wort_cool_time, ferment_cool_temp):
         self._id = id
         self._name = name
         self._volume = volume
+        self._yeast_store_amt = yeast_storage_amt
         self._yeast = yeast
+        self._yeast_begin_temp = yeast_begin_temp
         self._grain = grain
+        self._sparge_time = sparge_time
+        self._sparge_temp = sparge_temp
+        self._wort_volume = wort_volume
+        self._boil_temp = boil_temp
+        self._boil_time = boil_time
+        self._add_hop_time = add_hop_time
+        self._hop_amt = hop_amt
+        self._hops = hops
+        self._after_boil_chill_temp = after_boil_chill_temp
+        self._ferment_time = ferment_time
+        self._add_yeast_time = add_yeast_time
+        self._add_yeast_temp = add_yeast_temp
+        self._ferment_temp = ferment_temp
+        self._wort_cool_temp = wort_cool_temp
+        self._carbonation = carbonation
+        self._bitter_units = bitter_units
+        self._wort_cool_time = wort_cool_time
+        self._ferment_cool_temp = ferment_cool_temp
 
     def get_id(self):
         return self._id
@@ -34,11 +54,23 @@ class Recipe:
     def set_volume(self):
         return self._volume
 
-    def getYeast(self):
+    def get_yeast_storage_amt(self):
+        return self._yeast_store_amt
+
+    def set_yeast_storage_amt(self):
+        return self._yeast_store_amt
+
+    def get_yeast(self):
         return self._yeast
 
     def set_yeast(self):
         return self._yeast
+
+    def get_yeast_begin_temp(self):
+        return self._yeast_begin_temp
+
+    def set_yeast_begin_temp(self):
+        return self._yeast_begin_temp
 
     def get_grain(self):
         return self._grain
