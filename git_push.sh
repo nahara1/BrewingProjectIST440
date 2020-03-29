e76e0cbf5791f55ed78ed5098a7c67d538962bee
Prashant Patel <ppp5115@psu.edu>
2020-03-29 11:57:49 -0400

    latest upadate

---
+++ git_push.sh
@@ -0,0 +1,10 @@
+git add *
+git commit -m "latest update"
+git push https://prashant2412:Nadiad1234@github.com/prashant2412//BrewingProjectIST440.git master
+
+
+username = ?
+prashant2412
+
+password = ?
+Nadiad1234
