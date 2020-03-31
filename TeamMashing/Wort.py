b7ba02927fdb284cd57449417ead9617d139a1b0
Faith Gill <fya5021@psu.edu>
2020-03-31 19:47:46 -0400

    added pydoc to Wort.py

--- TeamMashing/Wort.py
+++ TeamMashing/Wort.py
@@ -3,27 +3,54 @@
 # Course: IST 440W
 # Author: Team Mashing
 # Date Developed: 3/17/2020
-# Last Date Changed: 3/24/2020
-# Rev: 1.2
+# Last Date Changed: 3/31/2020
+# Rev: 1.3
 
 import datetime
 from Log import Log
 
 
 class Wort:
-    def __init__(self, wv, hotWaterTemp, waterVolume):
-        self._wort_volume = wv
+    def __init__(self, wid, wortvolume, hotWaterTemp, waterVolume):
+        self.wort_id = wid
+        self._wort_volume = wortvolume
         self.HotWaterTemp = hotWaterTemp
         self.waterVolume = waterVolume
         self._wort_volume = False
         self.HotWater = False
 
-        try:
-            log = Log(1, "Wort.Received", "HotWater Received", datetime.datetime.now(), "Pass")
-            print(log.generate_log())
+    def wortVolume(self):
+        # Sets and displays wort volume
+        """
+        :type :float
+        :param float: shows correct wort volume
+        :return: Return log and animation of wort processes
+        """
+        # Records correct wort volume to Log and show animation
+        log = Log(2, "Mashing.Wort", "Wort Transferred", datetime.datetime.now(), "pass")
+        print(log.generate_log())
+        return "Wort Volume"
 
-            log = Log(2, "Mashing.Sparging", "Wort Generated", datetime.datetime.now(), "pass")
-            print(log.generate_log())
 
-        except Exception as e:
-            print(e)
+    def HotWaterTemp(self):
+        # Sets and displays the correct water temperature
+        """
+        :param int: displays correct water temp
+        :return :Return Log and animation of wort processes
+        """
+        # Records correct hot water temp to Log and show animation
+        log = Log(1, "Mashing.Wort", "Wort transferred", datetime.datetime.now(), "pass")
+        print(log.generate_log())
+        return "Displays Hot Water Temperature"
+
+    def water_volume(self):
+        # Displays the correct water volume
+        """
+        :param current_water_volume: get the correct water volume
+        :param set_correct_water_volume: set the correct water volume
+        :param :transfer correct water volume
+        :return: Return Log and animation of wort processes
+        """
+        log = Log(1, "Mashing.Wort", "Water Volume", datetime.datetime.now(), "pass")
+        print(log.generate_log())
+        return "Water Volume"
