1f5978699655df63a5e66e6214f23aacfb625647
Erik Ellis <elliserik69@gmail.com>
2020-03-31 08:24:57 -0400

    Updated boil functionality

--- TeamBoiling/Boil.py
+++ TeamBoiling/Boil.py
@@ -8,6 +8,7 @@
 
 import datetime
 import time
+import TeamBoiling.QualityCheck
 
 
 class Boil():
@@ -15,7 +16,8 @@ class Boil():
     _boil_time = time
     _boil_temp = 0.0
     _stage_date_time = datetime
-    _stage_duration = time
+    _stage_duration = datetime
+    _end_stage_date_time = datetime
     _is_boiling = bool
 
 
@@ -23,14 +25,14 @@ class Boil():
         self._boil_time
         self._boil_temp
         self._stage_date_time
-        self._stage_duration
+        self._stage_duration = datetime.datetime
         self._is_boiling
 
-    def __init__(self, _boil_time, _boil_temp, _stage_date_time, _stage_duration, _is_boiling):
+    def __init__(self, _boil_time, _boil_temp, _is_boiling):
         self._boil_time = _boil_time
         self._boil_temp = _boil_temp
-        self._stage_date_time = _stage_date_time
-        self._stage_duration = _stage_duration
+        self._stage_date_time = datetime.datetime.now()
+        self._stage_duration = datetime
         self._is_boiling = _is_boiling
 
     def get_boil_time(self):
@@ -54,7 +56,22 @@ class Boil():
         self._stage_duration = _stage_duration
 
     def start_boil(self):
-        return self.start_boil()
+        print("Start time: " + self._stage_date_time)
+        print("Boil time (mins): " + self._boil_time)
+        print("Boil temp: " + self._boil_temp)
+        print("Is it boiling? : " + self._is_boiling)
+        time.sleep(self._boil_time)
+        qaCheck = TeamBoiling.QualityCheck()
+        self.finish_boil(self, qaCheck)
+
+    def finish_boil(self, qaCheck):
+        print("QA Status: " + qaCheck)
+        self._end_stage_date_time = datetime.datetime.now()
+        print("End time: " + self._end_stage_date_time)
+        duration = self._end_stage_date_time - self._stage_date_time
+        self._stage_duration = duration.total_seconds()
+        print("Boil stage duration (mins): " + self._stage_duration)
+        print("Ending boil")
 
     def update_boil_status(self, _is_boiling):
         self._is_boiling = _is_boiling
