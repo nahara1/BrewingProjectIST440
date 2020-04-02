471062d8dab51863061b952bd661af7e0824bc67
Erik Ellis <elliserik69@gmail.com>
2020-04-02 08:19:46 -0400

    Added pydocs to TeamBoiling and BrewBatch and BrewBatchStage

--- TeamBoiling/Boil.py
+++ TeamBoiling/Boil.py
@@ -22,6 +22,10 @@ class Boil():
 
 
     def __init__(self):
+        '''
+        constructor method
+        :param self: allows access to methods and attributes
+        '''
         self._boil_time
         self._boil_temp
         self._stage_date_time
@@ -29,6 +33,14 @@ class Boil():
         self._is_boiling
 
     def __init__(self, _boil_time, _boil_temp, _is_boiling):
+        '''
+        overloads constructor method for parameters
+        :param _boil_time: boiling duration
+        :param _boil_temp: boiling temperature
+        :param _stage_date_time: timestamp for the stage
+        :param _stage_duration: duration of time during boil stage
+        :param _is_boiling: validates boiling
+        '''
         self._boil_time = _boil_time
         self._boil_temp = _boil_temp
         self._stage_date_time = datetime.datetime.now()
@@ -36,26 +48,70 @@ class Boil():
         self._is_boiling = _is_boiling
 
     def get_boil_time(self):
+        '''
+        gets the boiling time
+        :return: boiling time
+        '''
         return self._boil_time
+
     def set_boil_time(self, _boil_time):
+        '''
+        setter for boiling time
+        :param _boil_time: sets the boil time
+        :return: the boiling time after being set
+        '''
         self._boil_time = _boil_time
 
     def get_boil_temp(self):
+        '''
+        Gets the boiling temperature
+        :return: returns the boiling temperature
+        '''
         return self._boil_temp
+
     def set_boil_temp(self, _boil_temp):
+        '''
+        Setter for boiling temperature
+        :param _boil_temp: allows the temperature to be set
+        :return: the boiling temperature
+        '''
         self._boil_temp = _boil_temp
 
     def get_stage_date_time(self):
+        '''
+        gets the date and time of the boiling stage
+        :return: returns the stage date and time
+        '''
         return self._stage_date_time
+
     def set_stage_date_time(self, _stage_date_time):
+        '''
+        Setter for stage date and time
+        :param _stage_date_time: timestamp of the stage
+        :return: returns boiling stage date and time
+        '''
         self._stage_date_time = _stage_date_time
 
     def get_stage_duration(self):
+        '''
+        Gets the stage duration
+        :return: returns the time of the boiling stage
+        '''
         return self._stage_duration
+
     def set_stage_duration(self, _stage_duration):
+        '''
+        Setter for the duration of the boiling stage
+        :param _stage_duration: sets the duration of the boiling stage
+        :return: boiling stage time
+        '''
         self._stage_duration = _stage_duration
 
     def start_boil(self):
+        '''
+        Initiates the boiling process
+        :return: start of boiling process
+        '''
         print("Start time: " + self._stage_date_time)
         print("Boil time (mins): " + self._boil_time)
         print("Boil temp: " + self._boil_temp)
@@ -74,6 +130,11 @@ class Boil():
         print("Ending boil")
 
     def update_boil_status(self, _is_boiling):
+        '''
+        Updates boiling status from start to finish
+        :param _is_boiling: checks that boiling is in progress
+        :return: returns boiling status
+        '''
         self._is_boiling = _is_boiling
 
 
--- TeamBoiling/CloudHelper.py
+++ TeamBoiling/CloudHelper.py
@@ -3,9 +3,8 @@
 # Course: IST 440W - 001
 # Author: David Karminski(dck5200@psu.edu)
 # Date Developed: 3/18/20
-# Last Date Changed:
+# Last Date Changed: 3/31/20
 # Rev 1
-
 
 class CloudHelper():
 
@@ -14,17 +13,31 @@ class CloudHelper():
     _database_count = 1
 
     def __init__(self):
-        # self._local_database
+        #self._local_database
         self._database_type
         self._database_count
 
     def __init__(self, _database_type, _database_count, _local_database):
+        '''
+        Overloaded constructor method to initialize parameter and access attributes
+        :param _database_type: type of database to know where data is going
+        :param _database_count: counts the database
+        :param _local_database: local database
+        '''
         self._database_type = _database_type
         self._database_count = _database_count
         self._local_database = _local_database
 
     def get_database(self):
+        '''
+        Calls the data from the database
+        :return: returns the local database
+        '''
         return self._local_database
 
     def send_database(self):
+        '''
+        Sends data to the database
+        :return: returns the data to send to the database
+        '''
         return self.send_database()
