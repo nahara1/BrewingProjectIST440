9bfa9257cdd44dc5ffbefdf9ee56ff45fa867e29
annyespinal <anny_espinal@yahoo.com>
2020-04-06 18:24:56 -0400

    Added fermention vessel class

---
+++ TeamFerment/FermentationVessel.py
@@ -0,0 +1,21 @@
+#Project: IST 440 Barlog Brewery
+#Course: IST 440
+#Author: Anny Espinal (ace5139@psu.edu)
+#Date Developed: 4/6/20
+#Last Date Changed: 4/6/20
+#Rev: 1
+
+class FermentationVessel:
+    def __init__(self, vesselID, brewMasterID):
+        self.vesselID = vesselID
+        self.brewMasterID = brewMasterID
+
+    def closeLid(self):
+
+    def brewingComplete(self):
+
+    def drainAle(self):
+
+    def cleaningStatus(self):
+
+
