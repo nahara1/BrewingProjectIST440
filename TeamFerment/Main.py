85169b96ea78f065de84e8c06288f65c4c9f304f
annyespinal <anny_espinal@yahoo.com>
2020-04-06 17:50:46 -0400

    Added Main class

---
+++ TeamFerment/Main.py
@@ -0,0 +1,9 @@
+#Project: IST 440 Barlog Brewery
+#Purpose Details: To develop a main class that holds the information for the Fermentation process
+#Course: IST 440
+#Author: Anny Espinal (ace5139@psu.edu)
+#Date Developed: 4/6/20
+#Last Date Changed: 4/6/20
+#Rev: 1
+
+class Main:
