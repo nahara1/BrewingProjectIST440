4af1c5b6153d372befce8b4d4b6322523ccd13ca
teresabarker <teresa.barker12@gmail.com>
2020-04-06 19:29:23 -0400

    Adding Unit Test Folder and TestBoil.py file

---
+++ TeamBoiling/UnitTests/TestBoil.py
@@ -0,0 +1,15 @@
+# Project: Brewing Automation System - Capstone Project
+# Purpose Details: class for connecting between cloud and local databases
+# Course: IST 440W - 001
+# Author: Teresa Barker(tlb5767@psu.edu)
+# Date Developed: 4/1/2020
+# Last Date Changed: 4/6/20
+# Rev 3
+
+import unittest
+
+
+class TestBoil(unittest.TestCase):
+
+    if __name__ == '__main__':
+        unittest.main()
