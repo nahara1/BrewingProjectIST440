3d1d3119725a348132ff55999ce83e917741e6a3
Jarod Fagan <jdf5366@psu.edu>
2020-04-05 23:03:47 -0400

    Employee class setter and getter

--- TeamPrep/Employee.py
+++ TeamPrep/Employee.py
@@ -3,12 +3,31 @@
 # Course: IST 440W - 001
 # Author: TeamPrep
 # Date Developed: 3/23
-# Last Date Changed:3/23
+# Last Date Changed:4/5
 # Rev
 class Employee:
-    def __init__(self, employee_id, name, role):
+    def __init__(self, employee_id, employee_name, employee_role):
         self.employee_id = employee_id
-        self.name = name
-        self.role = role
+        self.employee_name = employee_name
+        self.employee_role = employee_role
 
+    def get_employee_id(self):
+        '''
+        gets the employee id
+        :return: employee id
+        '''
+        return self.employee_id
 
+    def get_employee_name(self):
+        '''
+        gets the employee name
+        :return: employee name
+        '''
+        return self.employee_name
+
+    def get_employee_role(self):
+        '''
+        gets the employee role
+        :return: employee role
+        '''
+        return self.employee_role
