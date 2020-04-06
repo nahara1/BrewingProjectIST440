f82890f573483778d0397d19ed663985088c8b50
DaiboZhang <dvz5048@psu.edu>
2020-04-05 20:12:40 -0400

    Taster test

--- TeamKegging/taster_class.py
+++ TeamKegging/taster_class.py
@@ -12,5 +12,12 @@ class TasterClass:  #Taster Class
         self.tc_id = tc_id
         self.tc_name = tc_name
         self.tc_role = tc_role
+
     def get_status(self):
-        return ""
+        return "Taster_ID: {}\n" \
+               "Taster_Name: {}\n" \
+               "Taster_Role: {}\n".format(self.tc_id, self.tc_name, self.tc_role)
+
+testTaster = TasterClass(12, "Jun Baek", "Quality Assurance 1")
+
+print(testTaster.get_status())
