c49d16886103cd83b357e5bd9ae911117cd5a7ca
AsraaAlkurdi <axa70@psu.edu>
2020-04-10 15:18:02 -0400

    Made corrections in the MongoLog class, added the BrewMaster class to the Brewing folder

--- Brewing/BrewMaster.py
+++ Brewing/BrewMaster.py
@@ -2,9 +2,9 @@
 # Purpose Details: To develop a Brew Master class that holds the information to the BrewMaster
 # Course: IST 440
 # Author: Team Ferment
-# Date Developed: 3/18/2020
-# Last Date Changed: 4/6/2020
-# Rev: 2
+# Date Developed: 4/8/2020
+# Last Date Changed: 4/8/2020
+# Rev: 1
 
 
 class BrewMaster:
@@ -12,15 +12,8 @@ class BrewMaster:
     This class has been written to obtain the Brew Master's needed credentials,
     as well as their corresponding Vessel ID.
     """
-    def __init__(self, _brew_master_id, _name, _vessel_id):
-        self._brew_master_id = _brew_master_id
-        self._name = _name
-        self._vessel_id = _vessel_id
-
-# def add_yeast(self):
-    # TODO: Add Code
-# def log_yeast(self):
-    # TODO: Add Code
-
-
-
+    def __init__(self, brew_master_id, name, vessel_id):
+        self.brew_master_id = brew_master_id
+        self.name = name
+        self.vessel_id = vessel_id
+# add getters
