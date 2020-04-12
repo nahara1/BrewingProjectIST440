20366e54a03904446c6216594cea2c034157236a
Christopher Parks <cvp5380@psu.edu>
2020-04-12 12:05:18 -0400

    updated customer.py
    added ServiceNOW code to get data from table

--- Brewing/Customer.py
+++ Brewing/Customer.py
@@ -36,18 +36,44 @@ class Customer:
         gets the customer address
         :return: customer address
         '''
-        return self.employee_address
+        return self.customer_address
 
     def get_customer_phone(self):
         '''
         gets the customer phone number
         :return: customer phone number
         '''
-        return self.employee_phone
+        return self.customer_phone
 
     def get_customer_email(self):
+       
         '''
         gets the customer email
         :return: customer email
         '''
-        return self.employee_email
+        return self.customer_email
+#Need to install requests package for python
+#easy_install requests
+import requests
+
+# Set the request parameters
+url = 'https://emplkasperpsu2.service-now.com/api/now/table/sys_user?sysparm_fields=first_name%2Clast_name%2Cemail%2Chome_phone%2Cstreet%2Ccity%2Cstate%2Czip&sysparm_limit=1'
+
+# Eg. User name="admin", Password="admin" for this code sample.
+user = 'IST440'
+pwd = 'IST440'
+
+# Set proper headers
+headers = {"Content-Type":"application/json","Accept":"application/json"}
+
+# Do the HTTP request
+response = requests.get(url, auth=(user, pwd), headers=headers )
+
+# Check for HTTP codes other than 200
+if response.status_code != 200:
+    print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
+    exit()
+
+# Decode the JSON response into a dictionary and use the data
+data = response.json()
+print(data)
