a8d16583cd149561b01f5688f92ae96855dceab6
Jarod Fagan <jdf5366@psu.edu>
2020-04-13 22:22:31 -0400

    Created get_employee class to pull data from service now (no url yet)

--- TeamPrep/Employee.py
+++ TeamPrep/Employee.py
@@ -31,3 +31,31 @@ class Employee:
         :return: employee role
         '''
         return self.employee_role
+
+
+class get_employee:
+#Need to install requests package for python
+#easy_install requests
+import requests
+
+# Set the request parameters
+url = ''
+
+# Eg. User name="admin", Password="admin" for this code sample.
+user = 'IST440'
+pwd = 'IST440'
+
+# Set proper headers
+headers = {"Content-Type":"application/json","Accept":"application/json"}
+
+# Do the HTTP request
+response = requests.get(url, auth=(user, pwd), headers=headers )
+
+# Check for HTTP codes other than 200
+if response.status_code != 200:
+    print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
+    exit()
+
+# Decode the JSON response into a dictionary and use the data
+data = response.json()
+print(data)
