2538f315a3d6518967fd404e7e001ab66c899a7d
aaleemsiddiqui <Bajisisnogulis1>
2020-04-14 15:44:23 -0400

    basic task list test

--- TeamKegging/KeggingTasks.py
+++ TeamKegging/KeggingTasks.py
@@ -1,10 +1,12 @@
 # Project: Brewing Automation System - Capstone Project
 # Purpose Details: Kegging - KeggingTask Class
 # Course: IST 440W - 001
-# Author: Ronald Salguero
+# Author: Aaleem
 # Date Developed: 4/4/2020
-# Last Date Changed:
-# Rev: 1
+# Last Date Changed: 4/14/2020
+# Rev: 2
+from pip._vendor.distlib.compat import raw_input
+
 
 class KeggingTasks:
     def __init__(self, task_id, task_category, task_prerequisite, task_status, task_confirmation):
@@ -14,19 +16,44 @@ class KeggingTasks:
         self.task_status = task_status
         self.task_confirmation = task_confirmation
 
-    def main():
-        t1 = 'task1'
-        t2 = 'task2'
-        t3 = 'task3'
-        t4 = 'task4'
-        t5 = 'task5'
-        list = [t1, t2, t3, t4, t5]
+    def Keggingtasksmain():
+        t1 = 'task 1'
+        t2 = 'task 2'
+        t3 = 'task 3'
+        t4 = 'task 4'
+        t5 = 'task 5'
+        t6 = 'task 6'
+        t7 = 'task 7'
+        t8 = 'task 8'
+        taskList = [t1, t2, t3, t4, t5, t6, t7, t8]
 
-        length = len(list)
+        length = len(taskList)
         i = 0
+        j = 0
+        k = 0
 
         while i < length:
-            print(list[i])
-            i += 1
+            print("list of tasks:")
+            print()
+            for i in range < length:
+                print(taskList[k] + " status: completed")
+                k += 1
 
-    main()
+            for i in range > length:
+                print(taskList[k] + " status: not completed")
+
+
+            print()
+            ans = raw_input("has " + taskList[i] + " been completed? Enter (y/n): ")
+            if ans == 'y':
+                i += 1
+                j += 1
+                k = 0
+            else:
+                k = 0
+                continue
+
+        print()
+        print("all kegging tasks completed.")
+
+    Keggingtasksmain()
