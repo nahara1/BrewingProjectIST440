882cc07b303be2223eb7863bd9ee8401e38a8436
Jarod Fagan <jdf5366@psu.edu>
2020-04-13 22:28:51 -0400

    Updated get_ingredients class to pull data from service now (no url yet)

--- TeamPrep/Employee.py
+++ TeamPrep/Employee.py
@@ -34,28 +34,28 @@ class Employee:
 
 
 class get_employee:
-#Need to install requests package for python
-#easy_install requests
-import requests
+    #Need to install requests package for python
+    #easy_install requests
+    import requests
 
-# Set the request parameters
-url = ''
+    # Set the request parameters
+    url = ''
 
-# Eg. User name="admin", Password="admin" for this code sample.
-user = 'IST440'
-pwd = 'IST440'
+    # Eg. User name="admin", Password="admin" for this code sample.
+    user = 'IST440'
+    pwd = 'IST440'
 
-# Set proper headers
-headers = {"Content-Type":"application/json","Accept":"application/json"}
+    # Set proper headers
+    headers = {"Content-Type":"application/json","Accept":"application/json"}
 
-# Do the HTTP request
-response = requests.get(url, auth=(user, pwd), headers=headers )
+    # Do the HTTP request
+    response = requests.get(url, auth=(user, pwd), headers=headers )
 
-# Check for HTTP codes other than 200
-if response.status_code != 200:
-    print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
+    # Check for HTTP codes other than 200
+    if response.status_code != 200:
+        print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
     exit()
 
-# Decode the JSON response into a dictionary and use the data
-data = response.json()
-print(data)
+    # Decode the JSON response into a dictionary and use the data
+    data = response.json()
+    print(data)
