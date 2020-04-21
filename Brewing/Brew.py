96e9b17f65b8638b5d506eca00f8305ed2fc47e6
nahara1 <narr.melo@gmail.com>
2020-04-21 12:47:28 -0400

    Fixed spelling

--- Brewing/Brew.py
+++ Brewing/Brew.py
@@ -71,7 +71,7 @@ def start_brew_batch(request_number, brew_stage, recipe):
 
     # Save log to MongoDB
 
-    # Create a BrewBatch object to be returned and passed along to the otehr brew phases
+    # Create a BrewBatch object to be returned and passed along to the other brew phases
     brew_batch = BrewBatch.BrewBatch(request_number, recipe, datetime.datetime.now(),
                                      brew_stage,
                                      "Prep Stage",
