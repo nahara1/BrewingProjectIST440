69668fc114f8342f875826ab77ceead5ed5d99d4
rachpocrass <rlp5382@psu.edu>
2020-04-23 22:10:32 -0400

    Updated headers

--- TeamMashing/MillingMachine.py
+++ TeamMashing/MillingMachine.py
@@ -3,8 +3,8 @@
 # Course: IST 440W
 # Author: Team Mashing
 # Date Developed: 3/17/2020
-# Last Date Changed: 4/21/2020
-# Rev: 3.0
+# Last Date Changed: 4/23/2020
+# Rev: 4.0
 
 import datetime
 import time
